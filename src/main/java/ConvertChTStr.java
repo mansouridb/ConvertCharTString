a42c9e86b9d38edcade82e9b6a8cd4b66c830d61
Majid <Mansouridb@gmail.com>
2021-04-22 12:22:00 +0200

    Initial commit

---
+++ src/main/java/ConvertChTStr.java
@@ -0,0 +1,7 @@
+public class ConvertChTStr {
+    public static void main(String[] args) {
+        char[] char1={'J','a','v','a'};
+        String str= new String (char1);
+        System.out.println(str);
+    }
+}
